99e61ef9ba7cfe6e40d5dd67ff4053d5064597e1
tejas salunkhe <salunkhetejas755@gmail.com>
2023-05-08 12:21:16 +0530

    appointment

--- src/main/java/com/example/book/apointment/controller/AppointmentController.java
+++ src/main/java/com/example/book/apointment/controller/AppointmentController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
 @RestController
+//@CrossOrigin(origins = http://localhost:3000/form)
 public class AppointmentController {
 
     @Autowired
